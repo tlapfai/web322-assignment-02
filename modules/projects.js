0aa981eb33466ecc8131fe8c5d5c35062d8bb3e4
Lap Fai Tam <tlapfai@hotmail.com>
2024-10-01 18:33:22 -0400

    coding completed

---
+++ modules/projects.js
@@ -0,0 +1,75 @@
+const projectData = require("../data/projectData");
+const sectorData = require("../data/sectorData");
+
+let projects = [];
+
+function initialize() {
+  //should resolve with no data, once the operation is complete (ie: the "projects" array is filled with objects)
+  /*projects = projectData.map((project) => ({
+    ...project,
+    sector: sectorData.find((sector) => sector.id === project.sector_id)
+      .sector_name,
+  }));
+  */
+  return new Promise((resolve, reject) => {
+    if (projectData.length > 0) {
+      projects = projectData.map((project) => ({
+        ...project,
+        sector: sectorData.find((sector) => sector.id === project.sector_id)
+          .sector_name,
+      }));
+    }
+    if (projects.length > 0) {
+      resolve();
+    } else {
+      reject("unable to initialize projects");
+    }
+  });
+}
+
+function getProjects() {
+  return new Promise((resolve, reject) => {
+    if (projects.length > 0) {
+      resolve(projects);
+    } else {
+      reject("unable to find projects");
+    }
+  });
+}
+
+function getProjectById(projectId) {
+  const project = projects.find((project) => project.id === projectId);
+  return new Promise((resolve, reject) => {
+    if (project) {
+      resolve(project);
+    } else {
+      reject("unable to find requested project");
+    }
+  });
+}
+
+function getProjectsBySector(sector) {
+  const project = projects.filter((project) =>
+    project.sector.toLowerCase().includes(sector.toLowerCase())
+  );
+  return new Promise((resolve, reject) => {
+    if (project.length > 0) {
+      resolve(project);
+    } else {
+      reject("unable to find requested project");
+    }
+  });
+}
+
+/*
+initialize();
+const temp = getProjectById(1);
+console.log(temp);
+*/
+
+module.exports = {
+  initialize,
+  getProjects,
+  getProjectById,
+  getProjectsBySector,
+};
---
+++ server.js
@@ -0,0 +1,59 @@
+/********************************************************************************
+ *  WEB322 – Assignment 02
+ *
+ *  I declare that this assignment is my own work in accordance with Seneca's
+ *  Academic Integrity Policy:
+ *
+ *  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+ *
+ *  Name: Lap Fai Tam Student ID: 126575232 Date: 2024-10-01
+ *
+ ********************************************************************************/
+
+const projectData = require("./modules/projects");
+
+const express = require("express");
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Server running on port ${HTTP_PORT}`);
+  projectData.initialize();
+});
+
+app.get("/", (req, res) => {
+  res.send("Assignment 2: Lap Fai Tam - 126575232");
+});
+
+app.get("/solutions/projects", (req, res) => {
+  projectData
+    .getProjects()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/solutions/projects/:id(\\d+)", (req, res) => {
+  projectData
+    .getProjectById(parseInt(req.params.id))
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/solutions/projects/:sector([a-zA-Z]+)", (req, res) => {
+  projectData
+    .getProjectsBySector(req.params.sector)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
